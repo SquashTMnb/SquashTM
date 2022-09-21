6adc201d096a64cf6202ad94f22fa8d98547ce19
SquashTMnb <113606194+SquashTMnb@users.noreply.github.com>
2022-09-21 10:05:06 +0200

    ajout fichiers
    ajout

---
+++ StartTFServer.bat
@@ -0,0 +1,1 @@
+C:\Users\nbouverat\Downloads\Automatisation_Installation\Robot_framework_TF\Debug\PM.TruckFlow.Servers.DebugServer.exe
---
+++ WinAppDriver.bat
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Windows Application Driver\WinAppDriver.exe" 127.0.0.1 4723
